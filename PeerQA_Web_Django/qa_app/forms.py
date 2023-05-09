c942352ff5b184bddc78195e213a01a9c2f0ca8a
pacifiic <yellowbill@snu.ac.kr>
2023-05-09 21:47:02 +0900

    Update: User to have student_id and nickname

--- PeerQA_Web_Django/qa_app/forms.py
+++ PeerQA_Web_Django/qa_app/forms.py
@@ -1,11 +1,24 @@
 from django import forms
-from .models import Question, Comment
+from .models import Question, Comment, User
+
+
+class SigninForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("student_id", "password")
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("student_id", "password", "nickname")
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ("title", "content", "status", "lecture_name", "lecture_slide", "postBy")  # '__all__'
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
--- PeerQA_Web_Django/qa_app/models.py
+++ PeerQA_Web_Django/qa_app/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # pip install psycopg2-binary
 # Create your models here.
+class User(AbstractUser):
+    student_id = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=50)
+
+
 class Question(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=400)
     status = models.CharField(max_length=12)
     lecture_name = models.CharField(max_length=24)
     lecture_slide = models.IntegerField()
-    postBy = models.CharField(max_length=12)
+    postBy = models.CharField(max_length=12) #models.ForeignKey(User, on_delete=models.CASCADE)
     postDate = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
