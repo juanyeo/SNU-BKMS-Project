177ec51f1b6c19bb150e094e331c1fb8520a5065
choimoonwon <yellowbill@snu.ac.kr>
2023-05-18 18:22:06 +0900

    tag 추가

--- PeerQA_Web_Django/qa_app/forms.py
+++ PeerQA_Web_Django/qa_app/forms.py
@@ -5,7 +5,7 @@ from .models import Question, Comment, User, Scrap
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ("title", "content", "status", "lecture_name", "lecture_slide", "user")  # '__all__'
+        fields = ("title", "content", "status", "lecture_name", "lecture_slide", "user", "tag")  # '__all__'
 
 
 class CommentForm(forms.ModelForm):
