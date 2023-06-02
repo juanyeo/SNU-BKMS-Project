55a3f1f304ad8d34e2f9e99e4a5498091d98dece
김권호 <gimgwonho@gimgwonhoui-MacBookAir.local>
2023-06-02 17:47:54 +0900

    graph visual 2

--- PeerQA_Web_Django/qa_app/neo4jconn.py
+++ PeerQA_Web_Django/qa_app/neo4jconn.py
@@ -36,7 +36,7 @@ class Database2():
             self.cursor = Neo4jConnection(
                 uri='bolt://localhost:7687',
                 user='neo4j',
-                pwd='12341234'
+                pwd='8752fm$$fm'
             )
 
     def __del__(self):
@@ -44,15 +44,18 @@ class Database2():
 
     def update(self):
         apoc_string = 'CALL apoc.load.jdbc("jdbc:postgresql://147.47.200.145:34543/teamdb6?user=team6&password=snupeer6",'
-        node1 = apoc_string + '"qa_app_user") YIELD row MERGE (u:User {id: toInteger(row.id), uname:row.username})'
+        node1 = apoc_string + '"qa_app_user") YIELD row MERGE (u:User {id: toInteger(row.id), uname:row.first_name})'
         node2 = apoc_string + '"qa_app_question") YIELD row MERGE (q:Question {id: toInteger(row.id), user_id:toInteger(row.user_id), tag:row.tag})'
         node3 = apoc_string + '"qa_app_comment") YIELD row MERGE (c:Comment {id: toInteger(row.id), question_id:toInteger(row.question_id), user_id:toInteger(row.user_id)})'
         rel1 = apoc_string + '"qa_app_user") YIELD row MATCH (u:User {id:toInteger(row.id)}) MATCH (q:Question {user_id:toInteger(row.id)}) MERGE (u)-[:ASK]->(q)'
         rel2 = apoc_string + '"qa_app_question") YIELD row MATCH (q:Question {id:toInteger(row.id)}) MATCH (c:Comment {question_id:toInteger(row.id)}) MERGE (c)-[:Relate]->(q)'
         rel3 = apoc_string + '"qa_app_user") YIELD row MATCH (u:User {id:toInteger(row.id)}) MATCH (c:Comment {user_id:toInteger(row.id)}) MERGE (u)-[:Answer]->(c)'
+        
+        query = 'MATCH (a:User)-[:ASK]->(:Question)<-[:Relate]-(:Comment)<-[:Answer]-(b:User) WHERE a.id <> b.id  WITH a.id as id1, b.id as id2, count(b.id)/6 as cnt MATCH (c:User{id:id1}), (d:User{id:id2}) MERGE (c)-[:Friends{strength:cnt}]->(d)'
         self.cursor.execute(node1)
         self.cursor.execute(node2)
         self.cursor.execute(node3)
         self.cursor.execute(rel1)
         self.cursor.execute(rel2)
         self.cursor.execute(rel3)
+        self.cursor.execute(query)
